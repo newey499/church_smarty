68fd5643f9451a7abd7166ac392f3f3546d50155
Chris Newey <newey499@hotmail.com>
2014-02-01 14:27:42 +0000

    delineate Smarty generated contented in index.php

--- index.php
+++ index.php
@@ -25,13 +25,16 @@ require_once('includes/class.SmartyExtended.php');
 
 $smarty = new SmartyExtended();
 
-$smarty->assign('name','Ned');
+$smarty->assign('name','Chris');
 
 //** un-comment the following line to show the debug console
 $smarty->debugging = true;
 
+print("========= Start Smarty generated content =========\n");
+
 $smarty->display('index.tpl');
 
+print("========= End Smarty generated content   =========<br>\n");
 ?>
 
 </body>
