59cb42e79063ee9099e6de6b78e3002bc0cd688f
Chris Newey <newey499@hotmail.com>
2014-02-03 08:59:14 +0000

    Add user bobiles to configDb.php. Create sql subdirectory and store backup of church database

--- configs/configDb.php
+++ configs/configDb.php
@@ -8,12 +8,18 @@ file: configDb.php
 
 class ConfigDb
 {
-	
+	/****************
 	const HOST = '127.0.0.1';
 	const USER = 'cdn';
 	const PASS = 'charlton';
 	const DB   = 'bobiles_church';	
-
+	*******************/
+	
+	const HOST = '127.0.0.1';
+	const USER = 'bobiles_church';
+	const PASS = 'charlton';
+	const DB   = 'bobiles_church';		
+	
 }
 
 
